dcf9c2f6096176fac828a5277866a56e3263057b
Luís Henrique <luis_alves@discente.ufg.br>
2023-12-10 20:49:39 -0300

    update 3.0
    alterações com uma possível solução para o escopo dinâmico

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Trabalho_BNL_LPP
+
 Trabalho da disciplina de Linguagens e Paradigmas de Programação 
+
+Bruno's no sense language
+
+Requisitos para a execução do proejto:
+    - Linguagem scheme instalada
+    - Distribuição Linux testadas (Ubunutu (apt), Fedora (dnf), PopOS (apt), Manajro (pacman))
+    - Testado em Windows 10 e Windows 11
+
+Instalação da Linguagem Scheme
+    - interpretador e ambiente: racketlang e racketserver
+
+
+
--- bnl.rkt
+++ bnl.rkt
@@ -1,41 +1,103 @@
-; input do buffer do teclado
-(define (parse input)
-  (define (skip-whitespace str)
-    (if (string-null? str)
-        ""
-        (let ((char (string-ref str 0)))
-          (if (char-whitespace? char)
-              (skip-whitespace (substring str 1))
-              str))))
+/*
+Para melhor leitura, deixe um espaço em branco de duas linhas entre as funções
 
-; input de strings
-  (define (parse-name str)
-    (let ((name ""))
-      (let loop ((str (skip-whitespace str)))
-        (if (string-null? str)
-            (values name "")
-            (let ((char (string-ref str 0)))
-              (if (or (char-alphabetic? char) (char=? char '-'))
-                  (loop (substring str 1))
-                  (values name str)))))))
+*/
 
-; input de valores numericos
-  (define (parse-numeric-value str)
-    (let ((name "") (number ""))
-      (let loop ((str (skip-whitespace str)))
-        (if (string-null? str)
-            (values name number "")
-            (let ((char (string-ref str 0)))
-              (if (or (char-numeric? char) (char-alphabetic? char))
-                  (loop (substring str 1))
-                  (values name number str)))))))
+(define (interpret program)
+  
 
-; chamada de funcao
-  (define (parse-func-call str)
-    (let-values (((name rest) (parse-name str)))
-      (if (string=? rest "")
-          (values name "")
-          (let-values (((args rest) (parse-numeric-values (substring rest 1))))
-            (values name args)))))
+  (define environment '())
+  
 
+  (define (get-value name env)
+    (cond
+      ((assq name env) => cdr) ; Procura a variável no ambiente local
+      ((null? env) (error "Variável não definida:" name)) ; Se não encontrar, gera um erro
+      (else (get-value name (cdr env) ))
+    )
+  ) ; Procura no ambiente pai
+  
+
+  (define (set-value! name value env)
+    (if (assq name env)
+        (set-cdr! (assq name env) value)
+        (set! environment (cons (cons name value) environment))
+    )
+  )
+
+
+  (define (evaluate-exp exp env)
+    (cond
+      ((number? exp) exp) ; Números são valores diretos
+      ((string? exp) exp) ; Strings são valores diretos
+      ((symbol? exp) (get-value exp env)) ; Variáveis
+      ((eq? (car exp) 'print) (display (evaluate-exp (cadr exp) env)) (newline)) ; Função print
+      ((eq? (car exp) 'function) ; Definição de função
+       (let ((func-name (cadr exp))
+             (params (caddr exp))
+             (local-vars (cadddr exp))
+             (body (cddddr exp)))
+         (set-value! func-name
+                     (lambda args
+                       (let ((new-env (append (map cons params args) (map (lambda (v) (cons v 0)) local-vars))))
+                         (evaluate-body body new-env))))
+         func-name))
+      ((eq? (car exp) 'begin) ; Sequência de comandos
+       (define (eval-sequence seq env)
+         (if (null? (cdr seq))
+             (evaluate-exp (car seq) env)
+             (eval-sequence (cdr seq) env)))
+       (eval-sequence (cdr exp) env))
+      ((eq? (car exp) 'return) ; Retorno de valor
+       (evaluate-exp (cadr exp) env))
+      ((eq? (car exp) 'if) ; Condicional
+       (if (evaluate-exp (cadr exp) env)
+           (evaluate-exp (caddr exp) env)
+           (evaluate-exp (cadddr exp) env)))
+      ((eq? (car exp) 'set!) ; Atribuição
+       (let ((var (cadr exp))
+             (value (evaluate-exp (caddr exp) env)))
+         (set-value! var value env)
+         '()))
+      (else
+       (let ((func (evaluate-exp (car exp) env))
+             (args (map (lambda (arg) (evaluate-exp arg env)) (cdr exp))))
+         (apply func args)))))
+  
+  (define (evaluate-body body env)
+    (if (null? (cdr body))
+        (evaluate-exp (car body) env)
+        (begin
+          (evaluate-exp (car body) env)
+          (evaluate-body (cdr body) env))))
+  
+  (define (interpret-program program)
+    (let ((main-func (cadr program)))
+      (evaluate-body (cddr main-func) environment)))
+  
+  (interpret-program program)
 )
+
+;; Exemplo de uso:
+
+(define programa-exemplo
+  '(function main < >
+     vars a b c
+     begin
+       (set! a 10)
+       (set! b 15.5)
+       (set! a b)
+       (set! b (+ 1 5.7))
+       (set! c (- 2.67 a))
+       (set! a (* b a))
+       (set! b (+ c -6))
+       (if (> a -10.5)
+           (set! a (f < b 5.5 >))
+           (set! a b))
+       (print a)
+       (print b)
+       (print c)
+       (return a)
+     end))
+
+(interpret programa-exemplo)
